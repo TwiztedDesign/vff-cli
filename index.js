4913c65930e833ad9b8ec0461f62a6c42ef4a40e
dok <dumansky@gmail.com>
2018-10-11 16:14:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const program   = require('commander'),
+      serve     = require('./lib/serve'),
+      login     = require('./lib/login');
+
+
+program
+    .version('0.0.1');
+program
+    .command('serve')
+    .description("Serve the current folder")
+    .option("-p, --port <port>", "set port")
+    .option("-d, --dir <path>", "set directory")
+    .option("-e, --entry <entry>", "set entry file")
+    .action(serve);
+
+
+program
+    .command('login')
+    .action(login);
+
+
+
+
+
+
+program.parse(process.argv);
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) program.help();
