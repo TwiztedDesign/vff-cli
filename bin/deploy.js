b10dbb152a56b7b2755142c70b1c308dec20115c
zivkaziv <Z15194806k>
2019-01-28 10:45:26 +0200

    Adding limitation on the deployed file size

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,16 +1,25 @@
-const utils     = require('../lib/utils');
-const logger    = require('../lib/logger');
-const vf        = require('../lib/vf');
-const config    = utils.config();
-const messages  = config.messages;
-const fs        = require('fs');
-const path      = require('path');
+const utils                         = require('../lib/utils');
+const logger                        = require('../lib/logger');
+const vf                            = require('../lib/vf');
+const config                        = utils.config();
+const messages                      = config.messages;
+const maxFileSizeForDeployInMb      = config.maxFileSizeForDeployInMb;
+const fs                            = require('fs');
+const path                          = require('path');
+const kbInMb                        = 1000000;
 
 const STATUS_ACTIVE     = 'active';
 const archiveName       = 'overlay';
 const archiveExtension  = 'zip';
 const archiveMime       = 'application/zip';
 
+
+function validateFileSize(filePath){
+    const stats = fs.statSync(filePath);
+    const fileSizeInBytes = stats.size;
+    const fileSizeInMegabytes = fileSizeInBytes / kbInMb;
+    return fileSizeInMegabytes > maxFileSizeForDeployInMb;
+}
 
 module.exports = function () {
     logger.run("Verifying");
@@ -21,16 +30,21 @@ module.exports = function () {
             utils.saveDescriptor(descriptor);
             logger.run('Compressing');
             utils.zip('**/!(*.'+ archiveExtension +')', archiveName ,() => {
-                logger.run('Uploading');
-                const file = fs.readFileSync(path.resolve('./') + '/' + archiveName + '.' + archiveExtension);
-
-                utils.upload(res.data.upload_url, file, archiveMime).then(() => {
-                    descriptor.status = STATUS_ACTIVE;
-                    vf.update(descriptor);
-                    utils.delete(path.resolve('./') + '/' + archiveName + '.' + archiveExtension);
-                }).catch(err => {
-                    logger.error(err);
-                }).then(logger.done);
+                let filePath = path.resolve('./') + '/' + archiveName + '.' + archiveExtension;
+                //Verify the file size is not above the limit
+                if(validateFileSize(filePath)){
+                    logger.error(messages.servedProjectFileSizeExceeded);
+                } else {
+                    logger.run('Uploading');
+                    const file = fs.readFileSync(filePath);
+                    utils.upload(res.data.upload_url, file, archiveMime).then(() => {
+                        descriptor.status = STATUS_ACTIVE;
+                        vf.update(descriptor);
+                        utils.delete(path.resolve('./') + '/' + archiveName + '.' + archiveExtension);
+                    }).catch(err => {
+                        logger.error(err);
+                    }).then(logger.done);
+                }
             });
         }).catch(err => {
             if(err.response.status === 401){
