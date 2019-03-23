8dfb8d4a3a39452c1ac75e43cc02f1c6b7cf6904
zivkaziv <Z15194806k>
2019-03-23 08:18:05 +0200

    modify to async await

--- bin/status.js
+++ bin/status.js
@@ -2,13 +2,12 @@ const logger        = require('../lib/logger');
 const vf            = require('../lib/vf');
 const utils         = require('../lib/utils');
 
-module.exports = function () {
-
-    vf.getUser().then(function(res){
-        logger.info("Logged in as: " + res.data.local.email);
-    }).catch(function(){
+module.exports = async function () {
+    try {
+        let user = await vf.getUser();
+        logger.info("Logged in as: " + user.data.local.email);
+    }catch (e) {
         logger.info("Not logged in");
-    });
-
+    }
     logger.info("Server URL: " + utils.getBaseUrl());
 };
